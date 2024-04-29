dd643812b66e260d16d99f341fcc97b907e20519
DOUGLAS AURELIO DE JESUS <douglas_a_de-jesus@estudante.sesisenai.org.br>
2024-04-29 16:23:54 -0300

    Dg javascript

---
+++ DG.js
@@ -0,0 +1,42 @@
+let dificuldade
+function dlc1(){
+    switch(dificuldade){
+    //caso 1 modo fácil até 10
+        case 1:
+            document.getElementById("Fácil") 
+            dificuldade = 10
+            //redirecionado para o modo fácil
+            break
+    //caso 2 modo médio até 50
+        case 2:
+        document.getElementById("Médio")
+        // redirecionar para o modo médio
+            dificuldade = 50
+            break
+    //caso 3 modo dificil até 100
+        case 3:
+            document.getElementById("Dificil")
+            // redirecionar para o modo dificil
+            dificuldade = 100
+            break
+                }
+}
+// let modo
+
+// document.getElementById("Fácil") = "";
+// document.getElementById("Médio") = "";
+// document.getElementById("Dificil") = "";
+
+// function facil(){
+//         }
+//     }
+
+// } 
+
+// function medio(){
+
+// }
+
+// function dificil(){
+    
+// }
